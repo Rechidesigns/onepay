7fa74690dde399a0a86f864cf9f5ef9ab4e5ae89
Rechidesigns <alerechiordu@gmail.com>
2023-06-30 16:20:13 +0100

    added admin view to wallet and wallettransaction

--- wallet/admin.py
+++ wallet/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from wallet.models import Wallet, WalletTransaction
 # Register your models here.
+
+@admin.register(Wallet)
+class WalletAdmin (admin.ModelAdmin):
+    list_display = ('user','currency', 'created_at',)
+    list_display_links = ('user','currency', 'created_at',)
+
+
+@admin.register(WalletTransaction)
+class WalletTransaction (admin.ModelAdmin):
+    list_display = ('wallet','transaction_type', 'amount','timestamp','status','paystack_payment_reference')
+    list_display_links = ('wallet','transaction_type', 'amount','timestamp','status')
